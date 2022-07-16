864a8aaff7b7ccddbce154ff75bf2cb0e2c2dd3a
Ellen Furness <elasfern@gmail.com>
2022-07-15 22:31:54 -0600

    WelcomePorfolio

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,21 +5,24 @@ function Nav() {
         <div class="card bg-dark text-white">
          
         <img src={process.env.PUBLIC_URL + '/assateague.png'} class="card-img" alt="Assateague Island" />
+        <h5> WELCOME TO MY PORFOLIO</h5>
+
         <div class="card-img-overlay">
-            <h5> WELCOME TO MY PORFOLIO</h5>
             
         </div>
         <nav class="navbar navbar-dark fixed-top bg-dark">
+
         <div>
         <ul>
-        <li>MY PORTFOLIO</li>
                         <li> <a href="#projects">Projects/Resume</a></li>
                         <li> <a href="#contact">Contact</a></li>
 
     
                     </ul>
         </div>
+
       </nav>
+
                 </div>
 
                 
