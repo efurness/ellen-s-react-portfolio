a738e8a2b968c5c8337ff013dfa7e5bbbea40ace
Ellen Furness <elasfern@gmail.com>
2022-07-23 19:27:57 -0600

    newResume

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -90,7 +90,7 @@ const Form = () => {
       </div>
       <ul>
                         
-                        <li class="list-inline-item"><a href="Furness_Resume_2022.pdf" target="blank"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/resume.png'}
+                        <li class="list-inline-item"><a href="FurnessPorfolioResume_2022" target="blank"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/resume.png'}
                     alt="resume_photo" /></a></li>
                         <li class="list-inline-item"><a href="https://stackoverflow.com/users/19552848/e-douglas?tab=profile"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/stack.png'}
                     alt="stack_photo" /></a></li>
