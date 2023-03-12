d289381f5481417e008fe8b4b3ffd75430397355
Ellen Furness <elasfern@gmail.com>
2023-03-11 21:57:24 -0700

    resumeInTwoPlaces

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,7 +14,7 @@ function Footer() {
                    
 <ul>
                         
-                        <li class="list-inline-item zoom"><a href="EDF_Resume_2023new.pdf" target="blank"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/resume.png'}
+                        <li class="list-inline-item zoom"><a href="Resume_2023_lab.pdf" target="blank"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/resume.png'}
                     alt="resume_photo" /></a></li>
                         <li class="list-inline-item zoom"><a href="https://stackoverflow.com/users/19552848/e-douglas?tab=profile"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/stack.png'}
                     alt="stack_photo" /></a></li>
