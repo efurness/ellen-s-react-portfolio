b27e67075f9a1036c096a7cfe62c59a70be74c29
Ellen Furness <elasfern@gmail.com>
2024-02-02 13:41:49 -0700

    story

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import './App.css';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Projects2 from './components/Projects2';
+// import Form from './components/Form';
+
+// eslint-disable-next-line
+import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter as 
+//   Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
+export const ThemeContext = createContext('null')
+// eslint-disable-next-line
+
+const projects = [
+  {
+    id: 1,
+    name: "Project 1",
+    title: 'City Scapes',
+    GitHub: <a href="https://github.com/jgault87/city-scapes.git">GitHub</a>,
+    Website: <a href="https://jgault87.github.io/city-scapes">Website</a>,
+    image: '/city_scapes.png',
+    
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    title: 'Fitness with Friends',
+    GitHub: <a href="https://github.com/AuraFly/Fit_With_Friends.git">GitHub</a>,
+    Website: <a href="https://fitwithfriends-app.herokuapp.com/">Heroku</a>,
+    image: '/fitness_friends.png',
+    
+
+  },
+  {
+    id: 3,
+    name:  <a href="https://www.youtube.com/watch?v=f4sIKWb2nYM">YouTube</a>,
+    title: 'Spokes People',
+    GitHub: <a href="https://github.com/bward3/spokes-people.git">Project 3 - Github</a>,
+    image: '/Spokes_people.png',
+
+  
+  },
+    
+];
+const projects2 = [
+
+  {
+    id: 4,
+    name: "Challenge",
+    title: 'Weather Dashboard',
+    GitHub: <a href="https://github.com/efurness/Unit-6-weather-dashboard.git">GitHub</a>,
+    Heroku: <a href="https://efurness.github.io/Unit-6-weather-dashboard/">Website</a>,
+    image: '/weather5.gif',
+    
+  },
+  {
+    id: 5,
+    name: "Challenge",
+    title: 'Note Taker Interface',
+    GitHub: <a href="https://github.com/efurness/note-taker-interface.git">GitHub</a>,
+    Heroku: <a href="https://pure-crag-28667.herokuapp.com/">Heroku</a>,
+    image: '/note_taker.png',
+  },
+  {
+    id: 6,
+    name: "Challenge",
+    title: 'Work Day Scheduler',
+    GitHub: <a href="https://github.com/efurness/Unit5-Work-Day-Scheduler.git">GitHub</a>,
+    Heroku: <a href="https://efurness.github.io/Unit5-Work-Day-Scheduler/">Website</a>,
+    image: '/work_day.png',
+
+  },
+];
+function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark': 'light'));
+  };
+  
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme}}>
+    
+    <div className='App' id={theme}>
+      <div className='switch2'>
+      <Nav />
+
+    <div className='switch'>
+      <lable> {theme === 'light' ? 'Light Mode' : 'Dark Mode'}</lable>
+    <ReactSwitch onChange={toggleTheme} checked={theme === 'light'} />
+    </div>
+    
+    {/* <Router>
+    <Routes>
+      <Route path="/" element={<Body />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/projects2" element={<Projects2 />}></Route>
+          <Route path="/contact" element={<Form />} ></Route>
+    </Routes>,
+    </Router> */}
+    <Body />,
+    <Projects Projects={projects}/>,
+        <Projects2 Projects={projects2}/>,
+        {/* <Form />, */}
+
+    <Footer />,
+    </div>
+    </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Body/index.js
@@ -0,0 +1,116 @@
+function Body() {
+
+    return (
+        
+        <body>
+
+        
+        
+    <div class="container px-4 px-lg-5" id="intro">
+    
+
+        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+            <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src={process.env.PUBLIC_URL + '/Montportfolio_bw.png'}
+                    alt="portfolio_photo" /></div>
+            <div class="col-lg-5">
+                
+                
+            <h3>Ellen Furness</h3>
+
+                <h5>Web development, Conservation, GIS, Population Biology, Biological Research</h5> 
+                
+                <br />
+            </div>
+        </div>
+        <div class="card text-white bg-secondary my-5 py-4 text-center">
+            <div class="card-body">
+                <p class="text-white m-0">
+                <p class="lead mb-0">The non-profit Conservation Treaty Support Fund, 
+                    <a href="https://www.prweb.com/releases/2007/02/prweb502845.htm">  CTFS</a> supported the <a href="https://cites.org/eng">Convention
+                    of the International Trade in Endangered Species of Wild Fauna and Flora </a> with partners
+                    <a href="https://worldwildlife.org"> World Wildlife Fund</a> and <a href="https://traffic.org"> TRAFFIC</a> in this pursuit, contributing to protecting species like
+                    elephants, rhinos and snow leopards: all vulnerable species because of poaching and illegal trade.
+                </p>
+                </p>
+            </div>
+        </div>
+        <div class="row gx-4 gx-lg-5">
+            <div class="col-md-4 mb-5">
+                
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title zoom">My education</h5>
+                        <p class="card-text">My education includes a Masters in Geographical Information Systems, at the University of Denver and Population Biology at the University of Colorado.
+                            GIS mapping projects. A Full-Stack JavaScript web development certificate.
+                        </p>
+                        <a href='https://efurness.github.io/Ellen-sProfessionalPortfolio/GIS2.pdf' class="btn btn-primary" >GIS Project</a>
+          
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5">
+                <div class="card">
+                <img src={process.env.PUBLIC_URL + '/peaks.png'} class="card-img-top" alt="Indian peaks" />
+                    <div class="card-body">
+                        <h5 class="card-title zoom">Colorado</h5>
+                        <p class="card-text">I love hiking with my English Retriever dog and enjoy cycling, snowshoeing, and skiing.</p>
+                        <a href="https://www.cblandtrust.org/" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-5" id="profession">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title zoom">Profession</h5>
+                        <p class="card-text">Work includes U.S. Forest Service
+                Burned Area Emergency Rehabiliation, GIS associate mapping Turkey Creek watershed with ArcGIS ArcInfo and ArcEdit,                                
+                and a work partnership with the National Park Service Air Resources Division.
+ </p>
+                         <br />
+                        <br />
+                        <a href="https://www.nps.gov/orgs/1971/index.htm" class="btn btn-primary">NPS Air websites</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container px-4 px-lg-5" id="projects">
+
+<div class="card bg-dark text-white">
+  <img src={process.env.PUBLIC_URL + '/sand.png'} class="card-img" alt="sand"></img>
+  <div class="card-img-overlay">
+
+
+    <p class="card-text zoom">
+                        <ul>
+                        <center>
+                            <li>ArcGIS Pro, ArcGIS Online, Python</li>        
+                            <li>FULL-STACK Web Development</li>                                                
+                            <li>REACT, MERN, JavaScript, Express</li>
+
+                            </center>
+                        </ul></p>
+
+
+
+  </div> 
+</div>
+< br />
+
+       <br />
+       <br />
+               <center> <h4>Projects and Challenges</h4></center>
+
+            </div>
+            
+    
+    
+    
+    
+    
+</body>
+    )
+}
+export default Body; 
