00a66c939ed8862f5d7d79423a4eb524aee18a25
Ellen Furness <elasfern@gmail.com>
2024-04-08 22:26:07 -0600

    again

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav() {
         <div class="card bg-dark text-white">
          
         <img src={process.env.PUBLIC_URL + '/gis2.png'} class="card-img" alt="blueimage" />
-        <li><h5> WELCOME TO MY REACT PORTFOLIO</h5></li>
+    <h5> WELCOME TO MY REACT PORTFOLIO</h5>
 
         <div class="card-img-overlay">
             
